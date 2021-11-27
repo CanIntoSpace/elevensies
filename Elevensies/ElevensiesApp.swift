8f8482cc564ae94c43d83022862d07b00620fa75
Michał Nierebiński <contact@caninto.space>
2021-11-27 15:07:31 +0100

    Freshly generated

---
+++ Elevensies/ElevensiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevensiesApp.swift
+//  Elevensies
+//
+//  Created by Michal Nierebinski on 27/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ElevensiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
